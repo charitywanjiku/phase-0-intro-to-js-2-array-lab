08bc39004420d9ff28af02a1650971ab96e2d402
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2023-12-17 19:20:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (){
+    cats.push("Ralph");
+    return (cats);
+}
+
+function destructivelyPrependCat (){
+    cats.unshift("Bob");
+    return(cats);
+}
+  
+function destructivelyRemoveLastCat (){
+    cats.pop("Ralph");
+    return(cats);
+}
+
+
+function destructivelyRemoveFirstCat (){
+    cats.shift("Bob");
+    return(cats);
+}
+
+function appendCat (George){
+   
+    return([...cats, George]);
+}
+
+function prependCat(Mark) {
+    return([Mark ,...cats]);
+
+}
+
+function removeLastCat() {
+    var newList = cats.slice(0, cats.length-1)
+    return(newList);
+}
+
+function removeFirstCat () {
+    var newestList = cats.slice(1)
+    return (newestList);
+}
